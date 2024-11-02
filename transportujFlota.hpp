eeda1b1fd6f8bff561066fad78a675c7d21e28c9
Zuzanna Żakowska <zakowska.zuzanna@gmail.com>
2024-11-02 11:45:59 +0100

    Zejsciowka 3

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,19 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar==0) return 0;
+
+    unsigned int suma = 0;
+    unsigned int nZaglowce = 0;
+
+    while (1){
+        Stocznia stocznia{};
+        Statek* s1 = stocznia();
+        if(dynamic_cast<Zaglowiec*>(s1)) ++nZaglowce;
+        unsigned int newTowar = s1->transportuj();
+        delete s1;
+        suma+=newTowar;
+        if (suma>=towar) {return nZaglowce;}
+    }
+
 }
